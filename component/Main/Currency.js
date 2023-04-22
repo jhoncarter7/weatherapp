c8630a6228624334c0bf5ae3a6f5f8626f89bb2b
Nikhil rai <rainikhil044@gmail.com>
2023-04-22 09:00:28 +0530

    final commit

---
+++ component/Main/Currency.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import classes from "./Widget.module.css"
+function Currency({key, updatedDate, CADkey, CADvalue, EURkey, EURvalue, USDkey, USDvalue}) {
+  return (
+    <div className={classes.main_component} key={key}>
+    <div
+      style={{
+        position: "absolute",
+        top: "16%",
+        display: "flex",
+        gap: "1rem",
+      }}>
+      
+    </div>
+
+    <div className={classes.currency}>
+      <h2>currency</h2>
+      <hr />
+
+      <ul>
+      <div className={classes.first_container}>
+        <li>On {updatedDate} {key} rate is</li>
+        <li> </li>
+        </div>
+        <div className={classes.sec_container}>
+        <li>{CADkey}: {CADvalue}, {EURkey}: {EURvalue}, {USDkey}: {USDvalue}</li>
+        </div> 
+      </ul>
+      <hr />
+      <ul>
+      <div className={classes.first_container}>
+        <li>On {updatedDate} {key} rate is</li>
+        <li> </li>
+        </div>
+        <div className={classes.sec_container}>
+        <li>{CADkey}: {CADvalue}, {EURkey}: {EURvalue}, {USDkey}: {USDvalue}</li>
+        </div> 
+      </ul>
+      <hr />
+      
+      <ul>
+      <div className={classes.first_container}>
+        <li>On {updatedDate} {key} rate is</li>
+        <li> </li>
+        </div>
+        <div className={classes.sec_container}>
+        <li>{CADkey}: {CADvalue}, {EURkey}: {EURvalue}, {USDkey}: {USDvalue}</li>
+        </div> 
+      </ul>
+      <hr />
+
+
+      <ul>
+      <div className={classes.first_container}>
+        <li>On {updatedDate} {key} rate is</li>
+        <li> </li>
+        </div>
+        <div className={classes.sec_container}>
+        <li>{CADkey}: {CADvalue}, {EURkey}: {EURvalue}, {USDkey}: {USDvalue}</li>
+        </div> 
+      </ul>
+      <hr />
+
+
+      <ul>
+      <div className={classes.first_container}>
+        <li>On {updatedDate} {key} rate is</li>
+        <li> </li>
+        </div>
+        <div className={classes.sec_container}>
+        <li>{CADkey}: {CADvalue}, {EURkey}: {EURvalue}, {USDkey}: {USDvalue}</li>
+        </div> 
+      </ul>
+      <hr />
+
+      <ul>
+      <div className={classes.first_container}>
+        <li>On {updatedDate} {key} rate is</li>
+        <li> </li>
+        </div>
+        <div className={classes.sec_container}>
+        <li>{CADkey}: {CADvalue}, {EURkey}: {EURvalue}, {USDkey}: {USDvalue}</li>
+        </div> 
+      </ul>
+      <hr />
+
+      <ul>
+      <div className={classes.first_container}>
+        <li>On {updatedDate} {key} rate is</li>
+        <li> </li>
+        </div>
+        <div className={classes.sec_container}>
+        <li>{CADkey}: {CADvalue}, {EURkey}: {EURvalue}, {USDkey}: {USDvalue}</li>
+        </div> 
+      </ul>
+      <hr />
+
+
+    </div>
+  </div>
+  )
+}
+
+export default Currency
--- component/Main/Widgets.js
+++ component/Main/Widgets.js
@@ -2,21 +2,17 @@ import React from "react";
 import classes from "./Widget.module.css";
 import { useSelector } from "react-redux";
 import Weather from "./Weather";
-
+import Currency from "./Currency";
 function Widgets() {
   const weatherData = useSelector((state) => state.widgets.weather);
   const currencyData = useSelector((state) => state.widgets.fetchCurrency);
    const cityCurrency = useSelector((state) => state.widgets.currency)
-  // console.log("store", cityName);
-  // console.log("store", Object.keys(currencyData));
-  // const somevalue = [currencyData.currenciesresp]
-  console.log(currencyData)
+
    
   return (
     <main className={classes.weatherCurrency}>
  {/* Weather data Table */}
-      {weatherData &&
-        weatherData.map((details) => (
+      {weatherData ? weatherData.map((details) => (
           <div className={classes.main_component} key={details.localtime_epoch}>
             <div
               style={{
@@ -45,107 +41,12 @@ function Widgets() {
               ))}
             </div>
           </div>
-        ))}
+        )) : <p>nothing</p>}
 
 {/* currency data Table */}
-      {currencyData? currencyData.map((currencys) => (
-          <div className={classes.main_component} key={cityCurrency}>
-            <div
-              style={{
-                position: "absolute",
-                top: "16%",
-                display: "flex",
-                gap: "1rem",
-              }}>
-              
-            </div>
-
-            <div className={classes.currency}>
-              <h2>currency</h2>
-              <hr />
-
-              <ul>
-              <div className={classes.first_container}>
-                <li>On {currencys.currenciesresp.meta.last_updated_at} {cityCurrency} rate is</li>
-                <li> </li>
-                </div>
-                <div className={classes.sec_container}>
-                <li>{currencys.currenciesresp.data.CAD.code}: {currencys.currenciesresp.data.CAD.value}, {currencys.currenciesresp.data.EUR.code}: {currencys.currenciesresp.data.EUR.value}, {currencys.currenciesresp.data.USD.code}: {currencys.currenciesresp.data.USD.value}</li>
-                </div> 
-              </ul>
-              <hr />
-              <ul>
-              <div className={classes.first_container}>
-                <li>On {currencys.currenciesresp.meta.last_updated_at} {cityCurrency} rate is</li>
-                <li> </li>
-                </div>
-                <div className={classes.sec_container}>
-                <li>{currencys.currenciesresp.data.CAD.code}: {currencys.currenciesresp.data.CAD.value}, {currencys.currenciesresp.data.EUR.code}: {currencys.currenciesresp.data.EUR.value}, {currencys.currenciesresp.data.USD.code}: {currencys.currenciesresp.data.USD.value}</li>
-                </div> 
-              </ul>
-              <hr />
-              
-              <ul>
-              <div className={classes.first_container}>
-                <li>On {currencys.currenciesresp.meta.last_updated_at} {cityCurrency} rate is</li>
-                <li> </li>
-                </div>
-                <div className={classes.sec_container}>
-                <li>{currencys.currenciesresp.data.CAD.code}: {currencys.currenciesresp.data.CAD.value}, {currencys.currenciesresp.data.EUR.code}: {currencys.currenciesresp.data.EUR.value}, {currencys.currenciesresp.data.USD.code}: {currencys.currenciesresp.data.USD.value}</li>
-                </div> 
-              </ul>
-              <hr />
-
-
-              <ul>
-              <div className={classes.first_container}>
-                <li>On {currencys.currenciesresp.meta.last_updated_at} {cityCurrency} rate is</li>
-                <li> </li>
-                </div>
-                <div className={classes.sec_container}>
-                <li>{currencys.currenciesresp.data.CAD.code}: {currencys.currenciesresp.data.CAD.value}, {currencys.currenciesresp.data.EUR.code}: {currencys.currenciesresp.data.EUR.value}, {currencys.currenciesresp.data.USD.code}: {currencys.currenciesresp.data.USD.value}</li>
-                </div> 
-              </ul>
-              <hr />
-
-
-              <ul>
-              <div className={classes.first_container}>
-                <li>On {currencys.currenciesresp.meta.last_updated_at} {cityCurrency} rate is</li>
-                <li> </li>
-                </div>
-                <div className={classes.sec_container}>
-                <li>{currencys.currenciesresp.data.CAD.code}: {currencys.currenciesresp.data.CAD.value}, {currencys.currenciesresp.data.EUR.code}: {currencys.currenciesresp.data.EUR.value}, {currencys.currenciesresp.data.USD.code}: {currencys.currenciesresp.data.USD.value}</li>
-                </div> 
-              </ul>
-              <hr />
-
-              <ul>
-              <div className={classes.first_container}>
-                <li>On {currencys.currenciesresp.meta.last_updated_at} {cityCurrency} rate is</li>
-                <li> </li>
-                </div>
-                <div className={classes.sec_container}>
-                <li>{currencys.currenciesresp.data.CAD.code}: {currencys.currenciesresp.data.CAD.value}, {currencys.currenciesresp.data.EUR.code}: {currencys.currenciesresp.data.EUR.value}, {currencys.currenciesresp.data.USD.code}: {currencys.currenciesresp.data.USD.value}</li>
-                </div> 
-              </ul>
-              <hr />
-
-              <ul>
-              <div className={classes.first_container}>
-                <li>On {currencys.currenciesresp.meta.last_updated_at} {cityCurrency} rate is</li>
-                <li> </li>
-                </div>
-                <div className={classes.sec_container}>
-                <li>{currencys.currenciesresp.data.CAD.code}: {currencys.currenciesresp.data.CAD.value}, {currencys.currenciesresp.data.EUR.code}: {currencys.currenciesresp.data.EUR.value}, {currencys.currenciesresp.data.USD.code}: {currencys.currenciesresp.data.USD.value}</li>
-                </div> 
-              </ul>
-              <hr />
-
-
-            </div>
-          </div>
-        )): <p>no currency</p>}
+      {currencyData ? currencyData.map((currencys) => (
+        <Currency key={cityCurrency} updateDate={currencys.currenciesresp.meta.last_updated_at} CADkey={currencys.currenciesresp.data.CAD.code} CADvalue={currencys.currenciesresp.data.CAD.value} EURkey={currencys.currenciesresp.data.EUR.code} EURvalue={currencys.currenciesresp.data.EUR.value} USDkey={currencys.currenciesresp.data.USD.code} USDvalue={currencys.currenciesresp.data.USD.value}/>
+      )) : <p>no currency</p>}
     </main>
   );
 }
